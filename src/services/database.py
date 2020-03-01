e157285a0b32dde3d36a21aea8f31f63bf8d187e
palunel <=>
2020-03-01 15:41:16 +0200

    Added feature #35

--- src/services/database.py
+++ src/services/database.py
@@ -1,4 +1,5 @@
 import pymongo as mongo
+import pandas as pd
 
 
 class Data:
@@ -6,15 +7,19 @@ class Data:
     def __init__(self, name):
         client = mongo.MongoClient("mongodb://localhost:27017")
         self._db = client[name]
-        pass
 
     def read_transactions(self):
-        pass
+        df = pd.DataFrame(self._db["transactions"].find({}))
+        df[["Labels"]] = df[["Labels"]].astype(int)
+        df[["Amount"]] = df[["Amount"]].astype(float)
+        return df
 
-    def persist_transactions(self):
-        pass
+    def persist_transactions(self, records):
+        self._db["transactions"].insert_many((records.astype('str')).to_dict('records'))
+        return self._db["transactions"].count_documents({})
 
     def calculate_diff(self):
         return 5
+
 
 data = Data("budget")
